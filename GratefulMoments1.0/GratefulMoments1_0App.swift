34674fafd935aa82b4d0015e294299b1af481a60
BWZ-CODE <mmige24@gmail.com>
2025-11-18 18:25:56 -0500

    Initial Commit

---
+++ GratefulMoments1.0/GratefulMoments1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMoments1_0App.swift
+//  GratefulMoments1.0
+//
+//  Created by Miguel Angel Bohorquez on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMoments1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
